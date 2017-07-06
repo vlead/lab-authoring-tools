e33cf580e0df8b93d2150b31f3b665ebe4cf6e7e
Projit_B <projit1234@hotmail.com>
2017-07-06 12:35:47 +0530

    can add images and move around

--- src/implementation/main.js
+++ src/implementation/main.js
@@ -1,12 +1,17 @@
 var newObject = function() {
-  alert("k");
-  $("#containment-wrapper").append("<div class='toDrag draggable ui-widget-content' style='border: 1px solid black'><p>Object</p></div>");
-  dragging();
+  var $url=$("#imgurl").val();
+  if ($url === "") return alert("Please Enter URL");
+  else
+  {
+    $("#containment-wrapper").append("<img class='toDrag draggableimage ui-widget-content' src='"+$url+"'/>");
+    dragging();
+  }
 };
 
 /*makes the object draggable*/
 var dragging = function() {
   $( ".toDrag" ).draggable({ containment: "#containment-wrapper", scroll: false });
+  $( ".toDrag" ).draggableimage({ containment: "#containment-wrapper", scroll: false });
 };
 
 
@@ -15,7 +20,3 @@ window.onload = function(){
   $("#btn").on("click", newObject);
   dragging();
 };
-
-
-
-
