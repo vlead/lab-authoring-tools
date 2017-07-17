159ed6fa4bd11a80cc82afc26aca29169d2fd24d
Projit_B <projit1234@hotmail.com>
2017-07-17 20:16:04 +0530

    reworking of documentation on implementation

--- src/implementation/main.js
+++ src/implementation/main.js
@@ -1,9 +1,10 @@
 var newObject = function() {
-  var $url=$("#imgurl").val();
+  var obj = {};
+  var $url = $("#imgurl").val();
   if ($url === "") return alert("Please Enter URL");
   else
   {
-    $("#containment-wrapper").append("<img class='toDrag draggableimage ui-widget-content' src='"+$url+"'/>");
+    $("#containment-wrapper").append("<img class='toDrag ui-widget-content' src='"+$url+"'/>");
     dragging();
   }
 };
@@ -11,12 +12,13 @@ var newObject = function() {
 /*makes the object draggable*/
 var dragging = function() {
   $( ".toDrag" ).draggable({ containment: "#containment-wrapper", scroll: false });
-  $( ".toDrag" ).draggableimage({ containment: "#containment-wrapper", scroll: false });
 };
+
 
 
 /* adding event listener */
 window.onload = function(){
   $("#btn").on("click", newObject);
   dragging();
+
 };
